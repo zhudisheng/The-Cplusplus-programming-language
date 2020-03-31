df8a8eddee60cc9865fb7146a37ae2e5affe415b
ZhuXinran <zhuran@mail.ustc.edu.cn>
2020-03-31 08:27:19 +0800

    overaload

---
+++ operatoroverload/a.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+class Complex
+{
+	int a;
+	int b;
+public:
+	Complex(int a = 0,int b = 0)
+	{
+		this->a = a;
+		this->b = b;
+	}
+	int getA()
+	{
+		return a;
+	}
+	int getB()
+	{
+		return b;
+	}
+	friend Complex Add(const Complex &p1,const Complex &p2);
+};
+Complex Add(const Complex &p1,const Complex &p2)
+{
+	Complex ret;
+	ret.a = p1.a + p2.a;
+	ret.b = p1.b + p2.b;
+	return ret;
+}
+int main()
+{
+
+	Complex c1(1, 2);
+	Complex c2(3, 4);
+	Complex c3 = Add(c1,c2);
+	printf("c1.a = %d,c1.b = %d\n",c1.getA(),c1.getB());
+	printf("c2.a = %d,c2.b = %d\n",c2.getA(),c2.getB());
+	printf("c3.a = %d,c3.b = %d\n",c3.getA(),c3.getB());
+	return 0;
+}
