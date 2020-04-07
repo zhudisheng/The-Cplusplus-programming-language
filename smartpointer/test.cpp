8ee31d008baa4444d9b59e722e5a35c7df7b8c64
ZhuXinran <zhuran@mail.ustc.edu.cn>
2020-04-07 09:16:44 +0800

    smartpointer

---
+++ smartpointer/test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <memory>
+using namespace std;
+
+class Test
+{
+	string m_name;
+public:
+	Test(const char* name)
+	{
+		cout << "Hello,"<<name<<"."<<endl;
+		m_name = name;
+	}
+	void print()
+	{
+		cout << "I'm"<<m_name<<"."<<endl;
+	}
+	~Test()	
+	{
+		cout << "Goodbye,"<<m_name<<"."<<endl;
+	}
+};
+int main()
+{
+	auto_ptr<Test> pt(new Test("Zhuxinran"));
+	cout << "pt = "<<pt.get()<<endl;
+	pt->print();
+	cout << endl;
+	auto_ptr<Test>pt1(pt);
+	cout << "pt = "<<pt.get()<<endl;
+	cout << "pt1 = "<<pt1.get()<<endl;
+	pt1->print();
+	return 0;
+}
+
